50cb3ddeb48a11aa1771fd5cd9e7b4b65b70934d
TPautras <tpautras@gmail.com>
2025-10-14 19:49:51 +0900

    feat: add flake template

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = ''
+    The average electrician config
+  '';
+
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, home-manager, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      overlays = import ./overlays { inherit inputs; };
+      nixosConfigurations = {
+        your-host = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/your-host ];
+        };
+      };
+      homeConfigurations = {
+        "your-name@your-host" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home/your-name/your-host.nix ];
+        };
+      };
+    };
+}
