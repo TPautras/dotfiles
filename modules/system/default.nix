1142312f4d9bde61a87d947ac0ace8a97670476b
TPautras <tpautras@gmail.com>
2025-10-17 22:21:15 +0900

    fix: wrong imports file

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,21 +1,29 @@
+{ lib, ... }:
+
+with lib;
+let
+  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+  getDir = dir: mapAttrs
+    (file: type:
+      if type == "directory" then getDir "${dir}/${file}" else type
+    )
+    (builtins.readDir dir);
+
+  # Collects all files of a directory as a list of strings of paths
+  files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
+
+  # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
+  importAll = dir: map
+    (file: ./. + "/${file}")
+    (filter
+      (file: hasSuffix ".nix" file && file != "default.nix" &&
+        ! lib.hasPrefix "x/taffybar/" file &&
+        ! lib.hasSuffix "-hm.nix" file)
+      (files dir));
+
+in
 {
-    pkgs,
-    config,
-    lib,
-    ...
-}:
-with lib; let
-    cfg = config.system.hyprland;
-in {
-    options.system.hyprland = {
-        enable = lib.mkEnableOption "Enable Hyprland system configuration";
-        description = ''Configures system settings for Hyprland'';
-    };
-    config = mkIf cfg.enable {
-        imports = [
-            ./wayland/wayland.nix
-            ./hyprland/default.nix
-            ./waybar/default.nix
-        ];
-    };
+
+  imports = importAll ./.;
+
 }
