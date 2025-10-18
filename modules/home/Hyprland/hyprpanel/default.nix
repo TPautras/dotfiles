690a37f15387c8d11c35ab8f4b210258f12acde6
TPautras <tpautras@gmail.com>
2025-10-18 18:59:02 +0900

    fix: add input in hyprpanel config

--- modules/home/Hyprland/hyprpanel/default.nix
+++ modules/home/Hyprland/hyprpanel/default.nix
@@ -13,6 +13,7 @@ in {
     };
     config = mkIf cfg.enable {
         programs.hyprpanel = {
+            package = inputs.hyprpanel.packages.${pkgs.system}.default;
             enable = true;
             # Configure and theme almost all options from the GUI.
             # See 'https://hyprpanel.com/configuration/settings.html'.
