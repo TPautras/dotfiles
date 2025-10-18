f017a4aaf263a995b101f426af8f592f04c5ad7e
TPautras <tpautras@gmail.com>
2025-10-19 00:48:50 +0900

    fix: stuff

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -16,6 +16,8 @@ in {
             substituters = ["https://hyprland.cachix.org"];
             trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
             experimental-features = [ "nix-command" "flakes" ];
+            max-jobs = 1;
+            cores = 5;
         };
     };
 }
