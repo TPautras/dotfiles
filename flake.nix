fbd8990b8a5e88b543c46e281abf0efe4aee183c
TPautras <tpautras@gmail.com>
2025-10-18 20:52:46 +0900

    idk

--- flake.nix
+++ flake.nix
@@ -23,8 +23,6 @@
         "aarch64-linux"
         "i686-linux"
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
