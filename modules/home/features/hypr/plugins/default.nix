aa8314adef6b1c094033de883a060f1e401cd687
Thomas Pautras <tpautras@gmail.com>
2026-07-20 13:18:13 +0200

    feat: add Azels hyprexpo

--- modules/home/features/hypr/plugins/default.nix
+++ modules/home/features/hypr/plugins/default.nix
@@ -1,31 +1,36 @@
 { self, inputs, ... }: {
   flake.homeManagerModules.hyprlandPlugins = { config, pkgs, lib, ... }:
   with lib; let
-    cfg = config.hm.hyprlandPlugins;
-    ef  = self.lib.palette;
-    hex = c: removePrefix "#" c;
+    cfg    = config.hm.hyprlandPlugins;
+    ef     = self.lib.palette;
+    hex    = c: removePrefix "#" c;
+    system = pkgs.stdenv.hostPlatform.system;
   in {
-    options.hm.hyprlandPlugins.enable = mkEnableOption "Hyprland plugins from nixpkgs (hyprspace overview)";
+    options.hm.hyprlandPlugins.enable = mkEnableOption "Hyprland plugins (hyprexpo overview)";
 
     config = mkIf cfg.enable {
       wayland.windowManager.hyprland = {
-        plugins = [ pkgs.hyprlandPlugins.hyprspace ];
+        plugins = [ inputs.hyprexpo-nix.packages.${system}.hyprexpo ];
 
-        settings.bind = [ "$mod, semicolon, overview:toggle" ];
+        settings.bind = [ "$mod, semicolon, hyprexpo:expo, toggle" ];
 
-        settings.plugin.overview = {
-          onBottom           = false;
-          centerAligned      = true;
+        settings.plugin.hyprexpo = {
+          columns          = 3;
+          gaps_in          = 5;
+          gaps_out         = 8;
+          bg_col           = "rgb(${hex ef.bg})";
+          workspace_method = "center current";
 
-          showEmptyWorkspace = true;
-          showNewWorkspace   = true;
+          show_cursor         = 1;
+          show_pinned_windows = 0;
 
-          exitOnClick        = true;
-          switchOnDrop       = true;
+          keynav_enable        = 1;
+          keynav_wrap_h        = 1;
+          keynav_wrap_v        = 1;
+          keynav_reading_order = 0;
 
-          panelColor              = "rgb(${hex ef.bg})";
-          workspaceActiveBorder   = "rgb(${hex ef.green})";
-          workspaceInactiveBorder = "rgb(${hex ef.bg2})";
+          cancel_key       = "escape";
+          gesture_distance = 200;
         };
       };
     };
--- modules/system/features/hyprland/default.nix
+++ modules/system/features/hyprland/default.nix
@@ -13,7 +13,11 @@
     };
 
     config = mkIf cfg.enable {
-      programs.hyprland.enable = true;
+      programs.hyprland = {
+        enable        = true;
+        package       = inputs.hyprland.packages.${pkgs.system}.hyprland;
+        portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      };
 
       xdg.portal = {
         enable       = true;
