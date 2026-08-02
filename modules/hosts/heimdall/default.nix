6689ac647cbff6a364ec92b9f7466a42a27b7862
Thomas Pautras <tpautras@gmail.com>
2026-08-03 01:55:13 +0200

    fix: monitors ?

--- modules/hosts/heimdall/default.nix
+++ modules/hosts/heimdall/default.nix
@@ -1,7 +1,10 @@
 { self, inputs, ... }: {
   flake.nixosConfigurations.heimdall = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    specialArgs = { inherit inputs; outputs = self; };
+    specialArgs = {
+      inherit inputs;
+      outputs = self;
+    };
     modules = [
       inputs.disko.nixosModules.disko
       self.nixosModules.heimdallHardware
@@ -9,6 +12,7 @@
       self.nixosModules.profileTower
       self.nixosModules.gaming
       self.nixosModules.heimdallConfig
+      self.homeManagerModules.heimdall
     ];
   };
 }
