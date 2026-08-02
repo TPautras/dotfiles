d0080f0c8e57b0ee0c02254e8413b6eea6fd42c9
Thomas Pautras <tpautras@gmail.com>
2026-08-03 01:32:58 +0200

    feat: add good monitors

--- modules/hosts/heimdall/config.nix
+++ modules/hosts/heimdall/config.nix
@@ -1,16 +1,44 @@
 { self, inputs, ... }: {
-  flake.nixosModules.heimdallConfig = { pkgs, lib, config, ... }: {
-    networking.hostName = "heimdall";
+  flake.nixosModules.heimdallConfig =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      networking.hostName = "heimdall";
 
-    sys = {
-      kernel.variant = "zen";
-      gaming.enable  = true;
+      sys = {
+        kernel.variant = "zen";
+        gaming.enable = true;
+      };
+
+      boot.loader.timeout = 0;
+
+      time.hardwareClockInLocalTime = true;
+
+      system.stateVersion = "25.05";
     };
-
-    boot.loader.timeout = 0;
-
-    time.hardwareClockInLocalTime = true;
-
-    system.stateVersion = "25.05";
+  flake.homeManagerModules.heimdall = { ... }: {
+    hm.hyprland.monitors = [
+      "desc:Dell Inc. DELL P2213 Y57VF2C7BPTM, 1680x1050@59.95, 0x15, 1"
+      "desc:Microstep MSI G255F BC0M305706374, 1920x1080@180, 1680x0, 1"
+      "desc:Microstep MSI G255F BC0M305706340, 1920x1080@180, 3600x0, 1"
+    ];
+    hm.hyprland.workspaceRules = [
+      # Dell — gauche
+      "1, monitor:desc:Dell Inc. DELL P2213 Y57VF2C7BPTM, default:true"
+      "2, monitor:desc:Dell Inc. DELL P2213 Y57VF2C7BPTM"
+      "3, monitor:desc:Dell Inc. DELL P2213 Y57VF2C7BPTM"
+      # MSI …374 — centre
+      "4, monitor:desc:Microstep MSI G255F BC0M305706374, default:true"
+      "5, monitor:desc:Microstep MSI G255F BC0M305706374"
+      "6, monitor:desc:Microstep MSI G255F BC0M305706374"
+      # MSI …340 — droite
+      "7, monitor:desc:Microstep MSI G255F BC0M305706340, default:true"
+      "8, monitor:desc:Microstep MSI G255F BC0M305706340"
+      "9, monitor:desc:Microstep MSI G255F BC0M305706340"
+    ];
   };
 }
