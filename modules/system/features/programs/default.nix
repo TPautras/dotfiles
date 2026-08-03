a56ac0b4464c3e1149c9965ac2f8703fe0dbdd2f
Thomas Pautras <tpautras@gmail.com>
2026-08-03 16:30:09 +0200

    feat: add ani-cli

--- modules/system/features/programs/default.nix
+++ modules/system/features/programs/default.nix
@@ -1,37 +1,46 @@
 { self, inputs, ... }: {
-  flake.nixosModules.programs = { config, pkgs, lib, ... }:
-  with lib; let
-    cfg = config.sys.programs;
-  in {
-    options.sys.programs.enable = mkEnableOption "base system CLI programs";
+  flake.nixosModules.programs =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    with lib;
+    let
+      cfg = config.sys.programs;
+    in
+    {
+      options.sys.programs.enable = mkEnableOption "base system CLI programs";
 
-    config = mkIf cfg.enable {
-      programs = {
-        fish.enable = true;
-        git.enable  = true;
-        nh = {
-          enable = true;
-          clean.enable = true;
-          clean.extraArgs = "--keep-since 4d --keep 3";
-          flake = "/home/${config.sys.user.name}/.dotfiles";
+      config = mkIf cfg.enable {
+        programs = {
+          fish.enable = true;
+          git.enable = true;
+          nh = {
+            enable = true;
+            clean.enable = true;
+            clean.extraArgs = "--keep-since 4d --keep 3";
+            flake = "/home/${config.sys.user.name}/.dotfiles";
+          };
         };
+        environment.systemPackages = with pkgs; [
+          vim
+          ani-cli
+          git
+          curl
+          discord
+          wget
+          unzip
+          p7zip
+          file
+          which
+          fastfetch
+          yazi
+          rustup
+          spotatui
+          inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+        ];
       };
-      environment.systemPackages = with pkgs; [
-        vim
-        git 
-        curl
-        discord 
-        wget 
-        unzip 
-        p7zip 
-        file 
-        which 
-        fastfetch
-        yazi 
-        rustup 
-        spotatui
-        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
     };
-  };
 }
